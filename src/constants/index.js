ddad4e00c6e0e350915a96ea0c4047cbd1cbfecb
chao wang <wc42888@hotmail.com>
2020-04-15 01:56:28 +1000

    updated constant.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 import { MAX_VEL, MIN_VEL } from './velocity';
+import { SLIDER_OFFSET, V_MAX, V_MIN } from './slider';
 
-export { MAX_VEL, MIN_VEL };
+export { MAX_VEL, MIN_VEL, SLIDER_OFFSET, V_MAX, V_MIN };
