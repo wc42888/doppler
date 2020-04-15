5c46b7efe9292c6d09aa5a63b66d2acb7575c57a
chao wang <wc42888@hotmail.com>
2020-04-15 12:36:30 +1000

    added constants for hue angles

---
+++ src/constants/hueColorAngle.js
@@ -0,0 +1,4 @@
+//  defines to hue rotate angles for
+//  blue and red color
+export const BLUE = 180;
+export const RED = -50;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 import { MAX_VEL, MIN_VEL } from './velocity';
 import { SLIDER_OFFSET, V_MAX, V_MIN } from './slider';
+import { BLUE, RED } from './hueColorAngle';
 
-export { MAX_VEL, MIN_VEL, SLIDER_OFFSET, V_MAX, V_MIN };
+export { MAX_VEL, MIN_VEL, SLIDER_OFFSET, V_MAX, V_MIN, BLUE, RED };
